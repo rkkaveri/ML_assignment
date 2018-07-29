bd55eedbd00d72a7ed122fcf322fbd94a8174a79
rkkaveri <rkkaveri.kk@gmail.com>
2018-07-29 19:40:20 +0530

    Introduction to ML

---
+++ ML_intro.py
@@ -0,0 +1,12 @@
+#Q1. From given list x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], produce an output of [8, 9, 10, 11, 12]
+#a) Do this by using slicing from the front (positive)
+#b) Do this by using slicing from the back (negative)  
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+print(x[7:12])#positive 
+print(x[-9:-4])#negative
+
+
+
+#Q2.Print even number from x using list slicing only. 
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+print(x[1::2])
